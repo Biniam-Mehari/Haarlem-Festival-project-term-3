05dbe4b356fa0fb710b2cdfbd1bafef7f06890b6
B-student <Bingoleley2015@gmail.com>
2022-02-11 09:39:04 +0100

    cms navigation

--- app/public/controller/cms.viewcontroller.php
+++ app/public/controller/cms.viewcontroller.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeController
+class CmsController
 {
     public function index()
     {
---
+++ app/public/view/cmsnavigation.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style1.css">
+    <title>Document</title>
+</head>
+<body>
+    
+     <a href="home">
+        <img class="logo" src="./img/logo.png" alt="" >
+        </a>
+    <nav>  
+    <div class="wrapper">
+      <ul>
+        <li><a href="home">Home</a></li>
+        <li><a href="BookView">Books</a></li>
+        <li><a href="contact">Contact</a></li>
+        <li><a href="signup">Become a member</a></li>
+       
+       <?php 
+         echo('<li><a class= "loginposition" href="login">LogIn</a></li>'); //<li><a class= "loginposition" href="login">LogIn</a></li>
+         echo('<li><img class="profile" src="img/default.profile.png" alt="profile" ></li>');// <img class="logo" src="img/default.profile.png" alt="profile" >
+       ?>
+      
+     
+       
+       
+      </ul>
+  
+    </div>
+  </nav>
+</body>
+</html>
