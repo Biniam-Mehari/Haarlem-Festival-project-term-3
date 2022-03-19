11ebd3595837781b48c18146e6ea8a7ea5d8cd7c
paxer2k <70654308+paxer2k@users.noreply.github.com>
2022-03-19 13:44:58 +0100

    changed navbar

--- app/public/view/foodmain.php
+++ app/public/view/foodmain.php
@@ -4,15 +4,25 @@ require_once 'navigation.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/foodmain.css">
     <title>Food</title>
-    <img class="bannerFoodMain" src="../img/bannerFoodMain.png" alt="bannerFoodMain">
 </head>
+
 <body>
-    
+    <section class="food-banner">
+        <img class="food-banner-image" src="../img/foodbanner.png">
+        <h1 class="food-title">Food</h1>
+        <p class="food-description">Taste some of the most exquisite cuisines that the Haarlem Festival has to offer!</p>
+    </section>
+
+    <section class="restaurants">
+        
+    </section>
 </body>
+
 </html>
--- app/public/view/navigation.php
+++ app/public/view/navigation.php
@@ -1,27 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style1.css">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/navigation.css">
+  <title>Document</title>
 </head>
+
 <body>
-    
-     <a href="/Haarlem-Festival-project-term-3/app/public/view/home.php">
-        <img class="logo"src="../img/logo.png" alt="" >
-        </a>
-    <nav>  
-    <div class="wrapper">
+  <div class="tasks-navigation">
+    <div class="navbar">
+      <a href="home">
+        <img src="../img/HFLogo.png" class="logo">
+      </a>
       <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="dance.php">Dance</a></li>
-        <li><a href="foodmain.php">Food</a></li>
-        <li><a href="history.php">History</a></li> 
+        <li><a href="home">Home</a></li>
+        <li><a href="tasks">Food</a></li>
+        <li><a href="about">Dance</a></li>
+        <li><a href="signup">History</a></li>
+        <li><a href="signup">Signup</a></li>
+        <?php if (isset($_SESSION['user'])) {
+          echo '<li><a href="logout">Logout</a></li>';
+        } else {
+          echo '<li><a href="login">Login</a></li>';
+        } ?>
       </ul>
-  
     </div>
-  </nav>
+  </div>
 </body>
+
 </html>
