9e4dc052cc8ed6e63e5f7b49a4a3d61c9358251b
paxer2k <70654308+paxer2k@users.noreply.github.com>
2022-03-23 22:16:24 +0100

    time values work, woohoo

--- app/public/view/foodreservation.php
+++ app/public/view/foodreservation.php
@@ -33,33 +33,63 @@ $restaurantInfo = $foodService->GetRestaurantById($restaurantID);
                 <img class="restaurant-image" src="../img/<?php echo $restaurantInfo->getImageName() ?>.png">
             </section>
         </section>
+    </section>
 
-        <br>
+    <br>
+    <br>
 
-        <section class="restaurant-reservation">
-            <form action="" method="post" class="form-container">
-                <h1 id="reservation-restaurant"><?php echo $restaurantInfo->getRestaurantName() ?></h1>
-                
-                <?php
-                $sessions = $restaurantInfo->getSessions();
-                $startDateTime = $restaurantInfo->getStartTime();
-                $durationPlus = $restaurantInfo->getDuration() * 60;
-                $startTime = strtotime($startDateTime);
-                $dayDuration = 60 * 60 * 24;
+    <?php
+    $sessions = $restaurantInfo->getSessions();
+    $startDateTime = $restaurantInfo->getStartTime();
+    $durationPlus = $restaurantInfo->getDuration() * 60;
+    $startTime = strtotime($startDateTime);
+    $dayDuration = 60 * 60 * 24;
 
+    ?>
 
-                for($day = 0; $day < 4; $day++) {
-                    for($session = 0; $session < $sessions; $session++) {
-                        $time = (($session * $durationPlus) + $startTime) + ($day * $dayDuration);
-                        $sessionTimes = date('d-m H:i', $time);
-                        echo $sessionTimes . " ";
-                        
-                    }
-                }
+    <section class="restaurant-booking">
+        <h1>Book your table</h1>
+    </section>
+    <section class="restaurant-reservation">
+        <form action="" method="post" class="form-container">
+            <div class="main-form">
+                <form action="index.php" method="get">
+                    <div>
+                        <span>Date and Time:</span>
+                        <select name="people" id="people" required>
+                            <?php for ($day = 0; $day < 4; $day++) {
+                                for ($session = 0; $session < $sessions; $session++) {
+                                    $time = (($session * $durationPlus) + $startTime) + ($day * $dayDuration);
+                                    $sessionTimes = date('d-m H:i', $time);
+                                    ?>
+                                    <option value="<?php echo $restaurantInfo->getStartTime() ?>"><?php echo $sessionTimes ?></option>
+                                    <?php }
+                            }?>
+                        </select>
+                    </div>
+                    <div>
+                        <span>Number of adults: </span>
+                        <input type="text" name="restaurant-adults" id="restaurant-adults" placeholder="Enter number of adults..." required>
+                    </div>
+                    <div>
+                        <span>Number of children: </span>
+                        <input type="text" name="restaurant-children" id="restaurant-children" placeholder="Enter number of children..." required>
+                    </div>
+                    <div>
+                        <span>Additional comments: </span>
+                        <textarea type="text" style="resize: none;" cols="100" rows="10" name="restaurant-comment" id="restaurant-comment" placeholder="Enter comment here..." required></textarea>
+                    </div>
+                    <div id="submit">
+                        <input type="submit" value="SUBMIT" id="submit">
+                    </div>
+                </form>
+            </div>
 
-                ?>
-            </form>
-        </section>
+            <?php
+
+            ?>
+        </form>
+    </section>
 </body>
 
 </html>
