9976fd5c3ca074e7b3342aa7d13e7807830271f3
paxer2k <70654308+paxer2k@users.noreply.github.com>
2022-05-03 20:40:35 +0200

    navigation fixed

--- app/views/components/navigation.php
+++ app/views/components/navigation.php
@@ -1,37 +1,41 @@
-<?php
-//session_start();
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Navigation</title>
-    <link rel="stylesheet" href="/css/navigation.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/navigation.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+  <title>Document</title>
 </head>
 
 <body>
-    <div class="festival-navigation">
-        <div class="navbar">
-            <a href="home">
-                <img src="/img/HFlogo.png" class="logo">
-            </a>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/food">Food</a></li>
-                <li><a href="/dance">Dance</a></li>
-                <li><a href="signup">History</a></li>
-                <?php if (isset($_SESSION['user'])) {
-                    echo '<li><a href="logout">Logout</a></li>';
-                } else {
-                    echo '<li><a href="login">Login</a></li>';
-                } ?>
-            </ul>
-        </div>
-    </div>
+  <div class="VVVfixed-top">
+
+    <a href="/">
+      <img class="logo" src="/img/logo.png" alt="">
+    </a>
+
+    <nav>
+      <div class="wrapper">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/food">Food</a></li>
+          <li><a href="/dance">Dance</a></li>
+
+
+          <?php
+          echo ('<li><a class= "loginposition" href="login">LogIn</a></li>'); //<li><a class= "loginposition" href="login">LogIn</a></li>
+          echo ('<li><img class="profile" src="..\img\defaultprofile.png" alt="profile" ></li>'); // <img class="logo" src="img/default.profile.png" alt="profile" >
+          ?>
+          
+        </ul>
+
+      </div>
+    </nav>
+  </div>
 </body>
 
 </html>
